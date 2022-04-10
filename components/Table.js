37b40899cd78eee5c2c5ac1be4be2c9483fc0c3d
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-04-10 21:58:50 +0530

    dep attempt 2

--- components/Table.js
+++ components/Table.js
@@ -1,6 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { useState, useEffect } from "react";
-import Pagination from "./Pagination";
+// import Pagination from "./Pagination";
 
 // const paginationButtonNums = [
 //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
