4991ce647f64a7348a0e5bf81731fbb9d573d257
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-04-23 13:10:38 +0530

    update

--- pages/payments.js
+++ pages/payments.js
@@ -37,7 +37,7 @@ const Payments = (props) => {
       .then((data) => {
         console.log("Payments", data);
         setPayments(
-          data.data.map((item) => {
+          data.data.paymentDataArr.map((item) => {
             return {
               ...item,
               action: item,
