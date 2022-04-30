640a6968fc8c18ffb8499d23302cdb049d980619
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-04-30 10:53:22 +0530

    new changes

--- pages/demoPrograms/DemoBatches.js
+++ pages/demoPrograms/DemoBatches.js
@@ -1,42 +1,37 @@
 import { useState, useEffect } from "react";
-import LayoutSidebar from "../components/LayoutSidebar";
-import Modal from "../components/Modal";
+import LayoutSidebar from "../../components/LayoutSidebar";
+import Modal from "../../components/Modal";
 import { RefreshIcon } from "@heroicons/react/outline";
 import toast from "react-hot-toast";
-import Table from "../components/Table";
-import FlyoutMenu from "../components/FlyoutMenu";
+import Table from "../../components/Table";
+import FlyoutMenu from "../../components/FlyoutMenu";
 
 import { format, parseISO } from "date-fns";
 
-const DemoBatches = () => {
+const DemoBatches = (props) => {
   const [viewAddModal, setViewAddModal] = useState(false);
   const [programs, setPrograms] = useState([]);
   const [demoBatches, setDemoBatches] = useState([]);
 
+  const [deleteLoading, setDeleteLoading] = useState(false);
+
   useEffect(() => {
     getAllPrograms();
-    getDemoBatches();
   }, []);
 
-  const getDemoBatches = async () => {
-    await fetch(`https://api.habuild.in/api/demobatches`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Data", data);
-        setDemoBatches(
-          data.demoBatches.map((item) => {
-            return {
-              name: item.name,
-              program_id: item.program_id,
-              status: item.status,
-              ad_id: item.ad_id,
-              start_date: format(parseISO(item.start_date), "PPpp"),
-              end_date: format(parseISO(item.end_date), "PPpp"),
-            };
-          })
-        );
-      });
-  };
+  useEffect(() => {
+    setDemoBatches(
+      props.demoBatches?.map((item) => {
+        return {
+          name: item.name,
+          program_id: item.program_id,
+          status: item.status,
+          ad_id: item.ad_id,
+          action: item,
+        };
+      })
+    );
+  }, [props.demoBatches?.length]);
 
   const getAllPrograms = async () => {
     await fetch(`https://api.habuild.in/api/program/`)
@@ -46,32 +41,40 @@ const DemoBatches = () => {
       });
   };
 
+  const deleteDemoBatch = async (demoBatch) => {
+    console.log("Demo Batch", demoBatch);
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+
+    setDeleteLoading(true);
+    await fetch(
+      // `https://api.habuild.in/api/demoprogram/delBatch?id=${demoBatch.id}`,
+      `http://localhost:4000/api/demobatches/delDemoBatch?id=${demoBatch.id}`,
+      {
+        method: "DELETE",
+      }
+    ).then((res) => {
+      setDeleteLoading(false);
+      props.getDemoBatches();
+      if (res.status == 404) {
+        toast.error("Demo Batch Not Deleted.");
+      } else {
+        toast.success("Demo Batch Deleted.");
+      }
+    });
+  };
+
+  const menuItems = [
+    {
+      name: "Delete",
+      onClick: (actionEntity) => {
+        deleteDemoBatch(actionEntity);
+      },
+    },
+  ];
+
   const columns = [
-    // {
-    //   title: "",
-    //   dataIndex: "isSelected",
-    //   key: "isSelected",
-    //   renderHeader: true,
-    //   headerRender: () => {
-    //     return (
-    //       <input
-    //         onChange={(e) => handleSelectAll(e.target.checked)}
-    //         className="mt-1 h-4 w-4 rounded-md border-gray-300 "
-    //         type="checkbox"
-    //       />
-    //     );
-    //   },
-    //   render: (isSelected) => {
-    //     return (
-    //       <input
-    //         onChange={(e) => handleSelect(isSelected.identifier)}
-    //         className="mt-1 h-4 w-4 rounded-md border-gray-300 "
-    //         type="checkbox"
-    //         checked={isSelected.value}
-    //       />
-    //     );
-    //   },
-    // },
     {
       title: "Name",
       dataIndex: "name",
@@ -92,22 +95,23 @@ const DemoBatches = () => {
       dataIndex: "ad_id",
       key: "ad_id",
     },
-    {
-      title: "Start Date",
-      dataIndex: "start_date",
-      key: "start_date",
-    },
-    {
-      title: "End Date",
-      dataIndex: "end_date",
-      key: "end_date",
-    },
+
     {
       title: "Actions",
       dataIndex: "action",
       key: "action",
-      render: (lead) => {
-        return <FlyoutMenu menuItems={[]}></FlyoutMenu>;
+      render: (actionEntity) => {
+        if (deleteLoading) {
+          return (
+            <RefreshIcon className="text-green animate-spin h-6 w-6 mx-auto" />
+          );
+        }
+        return (
+          <FlyoutMenu
+            actionEntity={actionEntity}
+            menuItems={menuItems}
+          ></FlyoutMenu>
+        );
       },
     },
   ];
@@ -123,10 +127,11 @@ const DemoBatches = () => {
         Add Demo Batch +
       </button>
 
-      <Table columns={columns} dataSource={demoBatches} />
+      <Table columns={columns} dataSource={demoBatches ? demoBatches : []} />
 
       <AddDemoBatchModal
-        refreshData={getDemoBatches}
+        refreshData={props.getDemoBatches}
+        demoProgram={props.demoProgram}
         programs={programs}
         modalOpen={viewAddModal}
         setModalOpen={setViewAddModal}
@@ -137,17 +142,15 @@ const DemoBatches = () => {
 
 const AddDemoBatchModal = (props) => {
   const [name, setName] = useState("");
-  const [programId, setProgramId] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [ad_id, setAd_id] = useState("");
+  const [startTime, setStartTime] = useState("");
+
   const [apiLoading, setApiLoading] = useState(false);
 
   const formSubmit = (e) => {
     e.preventDefault();
     setApiLoading(true);
 
-    if (!name || !programId || !startDate || !endDate) {
+    if (!name || !startTime) {
       alert("Please enter all details.");
       setApiLoading(false);
       return;
@@ -157,11 +160,11 @@ const AddDemoBatchModal = (props) => {
     myHeaders.append("Content-Type", "application/json");
     var raw = JSON.stringify({
       name,
-      program_id: programId,
-      start_date: startDate,
-      end_date: endDate,
-      ad_id: ad_id,
-      status: "INACTIVE",
+      start_time:
+        props.demoProgram.start_date.toString().split("T")[0] +
+        " " +
+        startTime +
+        ":00",
     });
 
     var requestOptions = {
@@ -170,7 +173,10 @@ const AddDemoBatchModal = (props) => {
       body: raw,
       redirect: "follow",
     };
-    fetch("https://api.habuild.in/api/demobatches", requestOptions)
+    fetch(
+      `http://localhost:4000/api/demoprogram/addBatch?id=${props.demoProgram.id}`,
+      requestOptions
+    )
       // fetch("http://localhost:4000/api/demobatches", requestOptions)
       .then((response) => response.text())
       .then((result) => {
@@ -178,7 +184,6 @@ const AddDemoBatchModal = (props) => {
         props.refreshData();
         props.setModalOpen(false);
         toast.success("Demo Batch Created");
-        console.log(result);
       })
       .catch((error) => {
         setApiLoading(false);
@@ -187,12 +192,16 @@ const AddDemoBatchModal = (props) => {
       });
   };
 
+  console.log(
+    "Get Time !!!!!@@@@"
+    // new Date(props.demoProgram.start_date).setTime(startTime)
+  );
+
   return (
     <Modal
       apiLoading={apiLoading}
       modalOpen={props.modalOpen}
       setModalOpen={props.setModalOpen}
-      actionText="Add Lead"
       hideActionButtons
     >
       <form
@@ -202,7 +211,7 @@ const AddDemoBatchModal = (props) => {
         }}
       >
         <h2 className="text-left text-xl font-bold text-gray-900">
-          Add Demo Batch
+          Add Demo Batch for {props.demoProgram.name}
         </h2>
 
         <div className="col-span-6 sm:col-span-3">
@@ -229,57 +238,16 @@ const AddDemoBatchModal = (props) => {
             htmlFor="first-name"
             className="block text-sm font-medium text-gray-700"
           >
-            Ad id
-          </label>
-          <input
-            value={ad_id}
-            onChange={(e) => setAd_id(e.target.value)}
-            type="text"
-            name="ad_id"
-            id="ad_id"
-            placeholder="Ad id"
-            className="mt-1 p-2 text-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="program"
-            className="block text-md font-medium text-gray-700"
-          >
-            Associated Program
-          </label>
-
-          <select
-            onChange={(e) => setProgramId(e.target.value)}
-            className="p-2 mt-1 block w-full shadow-sm border border-gray-200 rounded-md"
-          >
-            <option></option>
-            {props.programs.map((item) => {
-              return (
-                <option key={item.id} value={item.id}>
-                  {item.title}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-
-        <div className="col-span-6 sm:col-span-3">
-          <label
-            htmlFor="first-name"
-            className="block text-sm font-medium text-gray-700"
-          >
             Start Date
           </label>
           <input
-            onChange={(e) => setStartDate(e.target.value)}
-            type="date"
+            onChange={(e) => setStartTime(e.target.value)}
+            type="time"
             required
             className="mt-1 p-2 text-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
           />
         </div>
-        <div className="col-span-6 sm:col-span-3">
+        {/* <div className="col-span-6 sm:col-span-3">
           <label
             htmlFor="first-name"
             className="block text-sm font-medium text-gray-700"
@@ -292,7 +260,7 @@ const AddDemoBatchModal = (props) => {
             type="date"
             className="mt-1 p-2 text-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
           />
-        </div>
+        </div> */}
 
         <button
           className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:col-start-2 sm:text-sm"
---
+++ pages/leads/StopWACommModal.js
@@ -0,0 +1,103 @@
+import { useEffect, useState, Fragment } from "react";
+
+import { RefreshIcon } from "@heroicons/react/outline";
+
+import toast from "react-hot-toast";
+
+const StopWACommModal = (props) => {
+  const [apiLoading, setApiLoading] = useState();
+
+  const apiCall = (status) => {
+    setApiLoading(true);
+
+    const selectedLeadsIds = props.selectedLeads.map((item) => item.member_id);
+
+    // console.log("Selected Leads ids", selectedLeadsIds);
+
+    if (selectedLeadsIds.length == 0) {
+      setApiLoading(false);
+      props.setOpen(false);
+      toast.error("No Lead selected.");
+      return;
+    }
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({
+      lead_member_ids: selectedLeadsIds,
+      status,
+    });
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+    // fetch("http://localhost:4000/api/lead/updateLeadCommStatus", requestOptions)
+    fetch(
+      "https://api.habuild.in/api/lead/updateLeadCommStatus",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setApiLoading(false);
+        if (result.errorMessage) {
+          toast.error(result.errorMessage);
+        } else {
+          toast.success(result.message);
+        }
+        props.getPaginatedLeads();
+        props.setSelectedLeads([]);
+        props.setOpen(false);
+        // console.log(result);
+      })
+      .catch((error) => {
+        setApiLoading(false);
+        // toast.error(error);
+        console.log("error", error);
+      });
+  };
+
+  return (
+    // <Modal
+    //   apiLoading={apiLoading}
+    //   modalOpen={props.viewStopWACommModal}
+    //   setModalOpen={props.setViewStopWACommModal}
+    //   hideActionButtons
+    // >
+    <div className="bg-white overflow-hidden shadow rounded-lg mt-4">
+      <div className="px-4 py-5 sm:p-6">
+        <div className="flex flex-col space-y-4 p-4 mt-4">
+          <div>{props.selectedLeadsLength} people selected</div>
+
+          <button
+            disabled={apiLoading}
+            onClick={() => apiCall("PAUSED")}
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:col-start-2 sm:text-sm"
+            type="submit"
+          >
+            Stop WA Communication
+            {apiLoading && (
+              <RefreshIcon className="text-white animate-spin h-6 w-6 mx-auto" />
+            )}
+          </button>
+          <button
+            disabled={apiLoading}
+            onClick={() => apiCall("ACTIVE")}
+            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:col-start-2 sm:text-sm"
+            type="submit"
+          >
+            Start WA Communication
+            {apiLoading && (
+              <RefreshIcon className="text-white animate-spin h-6 w-6 mx-auto" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+
+    // </Modal>
+  );
+};
+
+export default StopWACommModal;
