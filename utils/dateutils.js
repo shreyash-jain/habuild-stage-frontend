2882eb0325769e8209b1973133b502afa34d0b18
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-07-16 18:02:13 +0530

    Taken out of service

--- utils/dateutils.js
+++
@@ -1,9 +0,0 @@
-export const addDaysToDate = (newDate, numOfDays) => {
-  try {
-    const date = new Date(newDate);
-    date.setDate(date.getDate() + Number(numOfDays));
-    return date;
-  } catch (err) {
-    console.log(err);
-  }
-};
--- utils/stringUtility.js
+++
@@ -1,7 +0,0 @@
-export const remove_backslash_characters = (str) => {
-  return str
-    ?.replace("\r", "\\r")
-    .replace("\n", "\\n")
-    .replace("\t", "\\t")
-    .split(/\\/)[0];
-};
