70b728e03b6b2f18b7968ff8769da36ab5ade245
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-07-19 22:00:16 +0530

    Added member Csv templates

--- pages/members/MemberCSVUpload.js
+++ pages/members/MemberCSVUpload.js
@@ -1,4 +1,4 @@
-import { RefreshIcon } from "@heroicons/react/outline";
+import { RefreshIcon, DownloadIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { MemberCSVApis } from "../../constants/apis";
@@ -13,7 +13,7 @@ const MemberCSVUpload = (props) => {
     if (!window.confirm("Are you sure?")) {
       return;
     }
-    
+
     setApiLoading(true);
     let API;
     let file;
@@ -31,6 +31,11 @@ const MemberCSVUpload = (props) => {
     if (calledFrom == "attendance") {
       API = MemberCSVApis.UPDATE_MEMBER_ATTENDANCE();
       file = attendanceFile;
+    }
+
+    if (!file) {
+      toast.error("Please select a file");
+      return;
     }
 
     let formData = new FormData();
@@ -71,13 +76,18 @@ const MemberCSVUpload = (props) => {
     );
   }
 
-  console.log(attendanceFile);
-  console.log(memberDataFile);
-
   return (
-    <div className="flex flex-col space-y-8 rounded-lg shadow p-6">
+    <div className="flex flex-col space-y-12 rounded-lg shadow p-6">
       <div className="flex flex-col space-y-2">
         <label className="font-medium text-gray-700">Update Member Data</label>
+        <a
+          href="/assets/Member_data_template.csv"
+          download={"Member_data_template.csv"}
+          className="text-gray-600 flex flex-row px-3 py-1 text-sm font-medium rounded-md max-w-fit hover:bg-gray-200"
+        >
+          <DownloadIcon className="w-4 h-4 mr-2" /> Download Member Data
+          Template
+        </a>
         <input
           onChange={(e) => setMemberDataFile(e.target.files[0])}
           type={"file"}
@@ -93,6 +103,14 @@ const MemberCSVUpload = (props) => {
         <label className="font-medium text-gray-700">
           Update Member Performance Data
         </label>
+        <a
+          href="/assets/Member_performance_template.csv"
+          download={"Member_performance_template.csv"}
+          className="text-gray-600 flex flex-row px-3 py-1 text-sm font-medium rounded-md max-w-fit hover:bg-gray-200"
+        >
+          <DownloadIcon className="w-4 h-4 mr-2" /> Download Member Performance
+          Template
+        </a>
         <input
           onChange={(e) => setPerformanceFile(e.target.files[0])}
           type={"file"}
@@ -108,6 +126,14 @@ const MemberCSVUpload = (props) => {
         <label className="font-medium text-gray-700">
           Update Member Attendance Data
         </label>
+        <a
+          href="/assets/Member_Attendance_template.csv"
+          download={"Member_Attendance_template.csv"}
+          className="text-gray-600 flex flex-row px-3 py-1 text-sm font-medium rounded-md max-w-fit hover:bg-gray-200"
+        >
+          <DownloadIcon className="w-4 h-4 mr-2" /> Download Member Attendance
+          Template
+        </a>
         <input
           onChange={(e) => setAttendanceFile(e.target.files[0])}
           type={"file"}
