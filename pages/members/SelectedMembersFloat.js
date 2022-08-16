8a842e69150036fd7a8c5321a208ab65611b0652
Japneet15597 <42543948+Japneet15597@users.noreply.github.com>
2022-08-16 15:40:06 +0530

    prod dep trigger

--- pages/members/SelectedMembersFloat.js
+++ pages/members/SelectedMembersFloat.js
@@ -194,6 +194,8 @@ const SelectedMembersFloat = (props) => {
                 calledFrom="groupActions"
                 customFetch={customFetch}
               />
+              
+              
 
               {/* <ChangeMemberChannel
                 modalOpen={changeChannelModal}
